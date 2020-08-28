A correction on the calculation of total wrench. Now the total wrench is derived from the motor thrusts instead of from the controller directly, which was incorrect. The control parameters are changed/tuned accordingly. The test on a structure is tested along with rank(A)=4 and rank(A)=6. The case rank(A)=5 is still under construction.

modquad_simulator/scripts/modquad_sim.py
@@ -84,7 +84,7 @@ def simulate():
 
     # TODO read structure and create a service to change it.
     structure4 = Structure(ids=['modquad01', 'modquad02', 'modquad03', 'modquad04'],
-                           quads=[Quad(), Quad(), Quad(), Quad()],
+                           quads=[Quad(np.pi/10), Quad(-np.pi/10, np.pi/8), Quad(), Quad()],
                            xx=[0, params.cage_width, 0, params.cage_width],
                            yy=[0, 0, params.cage_width, params.cage_width],
                            motor_failure=[])
@@ -93,8 +93,8 @@ def simulate():
                                xx=[0, params.cage_width, 0, params.cage_width],
                                yy=[0, 0, params.cage_width, params.cage_width],
                                motor_failure=[(1, 0)])
-    structure1 = Structure(ids=[robot_id], quads=[Quad()], xx=[0], yy=[0])
-    structure = structure1
+    structure1 = Structure(ids=[robot_id], quads=[Quad(np.pi/10, -np.pi/8)], xx=[0], yy=[0])
+    structure = structure4
 
     # Subscribe to control input
     rospy.Subscriber('/' + robot_id + '/cmd_vel', Twist, control_input_listener)
@@ -144,7 +144,7 @@ def simulate():
             # Overwrite the control input with the demo trajectory
         f_des = geo_position_controller(state_vector, desired_state)
 
-        print demo_trajectory, f_des
+        # print demo_trajectory, f_des
         # Control output based on crazyflie input
         tau_des = geo_attitude_controller(f_des, state_vector, desired_state)
 

modquad_simulator/src/modsim/attitude.py
@@ -32,7 +32,7 @@ def geo_attitude_controller(f_des, state_vector, desired_state):
     R = quaternions.quat2mat(quat)
     omega = np.array(state_vector[10:])
 
-    kp, kd, ki = 1.43e-5 * 250, 1.43e-5 * 60, .0002
+    kp, kd, ki = 1.43e-5 * 1200, 1.43e-5 * 540, .00001
 
     # desired direction in SO(3)
     # Eq 6a - 6e